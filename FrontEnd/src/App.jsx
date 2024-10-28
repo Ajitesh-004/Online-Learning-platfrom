64f9fa166a1b019454bec6dac2170cfe1f95e2a3
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-10-28 22:07:07 +0530

    Initial Frontend with empty files

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500">
+      <h1 className="text-purple-50">Hello, Tailwind!</h1>
+    </div>
+  );
+}
+
+export default App;
