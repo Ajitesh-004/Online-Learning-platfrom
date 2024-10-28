3db9d3971adb0abf2546b61fee422627c9fbdece
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-10-29 01:42:37 +0530

    middleware user authorization

---
+++ BackEnd/Middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+
+exports.authMiddleware = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(401).json({ message: 'Token not provided, unauthorized' });
+    }
+
+    const token = authHeader.split(' ')[1];
+    console.log(token);
+
+    try{
+        const decoded = jwt.verify(token,process.env.JWT_KEY);
+        req.user = decoded;
+        next();
+    } catch(error) {
+        return res.status(401).json({ message: 'Invalid token' });
+    }
+}
