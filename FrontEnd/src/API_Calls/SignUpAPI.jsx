697c1f2eb0245b2a8fa48592a944dfc582b87e24
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-10-29 15:39:32 +0530

    SignUp API to hit backend

---
+++ FrontEnd/src/API_Calls/SignUpAPI.jsx
@@ -0,0 +1,18 @@
+export const signup = async (username, email, password) => {
+    const response = await fetch('http://localhost:3000/api/signup', {
+        method: 'POST',
+        headers : {
+            'content-type': 'application/JSON'
+        },
+        body: JSON.stringify({username, email, password})
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+        const errorMessage = data.message || 'An error occurred during signup.';
+        throw new Error(errorMessage);
+    }
+
+    return data.message;
+}
