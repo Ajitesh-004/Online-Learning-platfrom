8d4e5f89cc496c7dc624977a5254446ea0f00fdd
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-10-30 00:25:01 +0530

    updated index.js file

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -2,25 +2,39 @@ import express from 'express';
 import cors from 'cors';
 import connectDB from './db/index.js';
 import dotenv from 'dotenv';
-import { Route }  from './routes/userRoute.js'
-import { authMiddleware } from './Middlewares/authMiddleware.js'
+import UserRoute from './routes/userRoute.js';
+import { authMiddleware } from './Middlewares/authMiddleware.js';
 
 dotenv.config();
-const app = express();
-app.use(express.json());
-app.use(cors());
 
+// Connect to the database
 connectDB();
 
-app.use('/', (req,res) => {
+const app = express();
+
+
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN || '*', 
+    optionsSuccessStatus: 200 
+};
+app.use(cors(corsOptions));
+
+app.use(express.json())
+
+app.use('/api', UserRoute);
+
+app.get('/', (req, res) => {
     res.status(200).send("Hello from Express!");
 });
 
-app.use('/api',authMiddleware,Route)
-
+// Error handling middleware should be the last middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
 
-app.listen(process.env.PORT || 3000);
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
