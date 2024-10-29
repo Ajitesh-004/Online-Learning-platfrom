cdfd3cc085517ccbc0b43dc8770c2fac9f03b320
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-10-30 00:31:19 +0530

    App.jsx and main.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,8 +1,29 @@
+import React, { Profiler } from 'react';
+import { useRecoilValue } from 'recoil';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Home } from './Components/Home';
+import { userAtom } from './atoms/userAtom'
+import { Navbar } from './Components/Navbar';
+import { SignUp } from './Components/SignUp';
+import { Login } from './Components/Login';
+import { UserProfile } from './Components/Profile'
+
 function App() {
+
+  const user = useRecoilValue(userAtom);
+
   return (
-    <div className="h-screen flex items-center justify-center bg-blue-500">
-      <h1 className="text-purple-50">Hello, Tailwind!</h1>
-    </div>
+    <>
+      <Navbar ></Navbar>
+      <div className="container mx-auto p-4">
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<SignUp />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/profile' element={<UserProfile />}/>
+        </Routes>
+      </div>
+    </>
   );
 }
 
--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css'; 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <RecoilRoot>
+    <Router>
+      <App />
+    </Router>
+  </RecoilRoot>
+);
+
+
