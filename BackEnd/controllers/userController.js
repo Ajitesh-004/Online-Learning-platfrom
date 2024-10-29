5490a6634f7fb6c7dd9c4e40bb612dcfb83cd47c
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-10-30 00:07:26 +0530

    updated user controller

--- BackEnd/controllers/userController.js
+++ BackEnd/controllers/userController.js
@@ -1,33 +1,33 @@
-import { validateUser, Users } from "../models/user.js";
+import Users, { validateUser } from '../models/User.js'; 
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import dotenv from'dotenv';
+import dotenv from 'dotenv';
 dotenv.config();
 
 export const SignUp = async (req, res) => {
-    const validate = validateUser.safeParse(req.body);
+    const validationResult = validateUser.safeParse(req.body);
 
-    if (!validate.success) {
-        return res.status(400).json({ message: validate.error.errors });
+    if (!validationResult.success) {
+        return res.status(400).json({ message: validationResult.error.errors });
     }
 
-    const { username , email , password } = validate.data;
+    const { username, email, password } = validationResult.data;
 
     try {
-        const existingUser = await Users.findOne({email});
+        const existingUser = await Users.findOne({ email });
 
         if (existingUser) {
-            return res.status(400).json({ message: 'User already exists'});
+            return res.status(400).json({ message: 'User already exists' });
         }
 
-        const hashedPassword = await bcrypt.hash(password,10);
+        const hashedPassword = await bcrypt.hash(password, 10);
 
-        const newUser = new Users({ username, email, password: hashedPassword});
+        const newUser = new Users({ username, email, password: hashedPassword });
         await newUser.save();
-        return res.status(201).json("New user created");
-    } catch(error) {
+        return res.status(201).json({ message: "New user created" });
+    } catch (error) {
         console.error(error);
-        return res.status(500).json({ message: `Internal server error ${error}`});
+        return res.status(500).json({ message: `Internal server error: ${error}` });
     }
 };
 
@@ -37,18 +37,18 @@ export const Login = async (req, res) => {
     try {
         const user = await Users.findOne({ email });
         if (!user) {
-            return res.status(404).json({ message: "No user with this email"});
+            return res.status(404).json({ message: "No user with this email" });
         }
 
-        const isMatch = await bcrypt.compare(password,user.password);
+        const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
-            return res.status(401).json({ message: "Invalid password "});
+            return res.status(401).json({ message: "Invalid password" });
         }
 
-        const token = jwt.sign({ userId: user._id}, process.env.JWT_KEY);
-        return res.status(200).json({ username: user.username, email: user.email, token })
-    } catch(error) {
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_KEY);
+        return res.status(200).json({ username: user.username, email: user.email, token });
+    } catch (error) {
         console.error(error);
-        return res.status(500).json({ message: `Internal server error ${error}`});
+        return res.status(500).json({ message: `Internal server error: ${error}` });
     }
-}
+};
