c8eecb0eb0a05c1f698814925ccbaf1502e70d14
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-10-30 00:25:31 +0530

    updated user route

--- BackEnd/routes/userRoute.js
+++ BackEnd/routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { Login, SignUp } from '../controllers/userController.js';
+
 const Route = express.Router();
 
-Route.post('/auth/signup', SignUp);
-Route.post('/auth/login', Login);
+Route.post('/signup', SignUp);
+Route.post('/login', Login);
 
-export { Route };
+export default Route; // Directly export the Route
