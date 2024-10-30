1a3446efe176bb3025bb63ef7c599872d1bb044b
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-10-30 14:57:49 +0530

    Updated routes

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -6,20 +6,10 @@ import UserRoute from './routes/userRoute.js';
 import { authMiddleware } from './Middlewares/authMiddleware.js';
 
 dotenv.config();
-
-// Connect to the database
-connectDB();
-
 const app = express();
-
-
-const corsOptions = {
-    origin: process.env.CORS_ORIGIN || '*', 
-    optionsSuccessStatus: 200 
-};
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(express.json())
+connectDB();
 
 app.use('/api', UserRoute);
 
@@ -27,13 +17,11 @@ app.get('/', (req, res) => {
     res.status(200).send("Hello from Express!");
 });
 
-// Error handling middleware should be the last middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
