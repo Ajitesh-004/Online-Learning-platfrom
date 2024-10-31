9c4ff18d7842cfb98ef4f88d842ce1fcce588096
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-10-31 13:14:06 +0530

    updated profile atom

--- FrontEnd/src/atoms/profileAtom.jsx
+++ FrontEnd/src/atoms/profileAtom.jsx
@@ -1,9 +1,6 @@
 import { atom } from 'recoil';
-import Profile from '../assets/DefaultProfile.jpg';
 
 export const profileAtom = atom({
     key: 'profileAtom', 
-    default: {
-        profilePicture: Profile, 
-    },
+    default: null,
 });
