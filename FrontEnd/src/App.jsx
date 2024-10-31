e894894bd540df901815d8212fb7e09690e9b621
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-10-31 13:18:51 +0530

    Updated App.jsx with search bar, nav bar, profile component

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -7,19 +7,25 @@ import { Navbar } from './Components/Navbar';
 import { SignUp } from './Components/SignUp';
 import { Login } from './Components/Login';
 import { UserProfile } from './Components/Profile'
+import Contact from './Components/Contact';
+import { SearchResults } from './Components/SearchResults';
+import { searchQueryAtom } from './atoms/searchQueryAtom';
 
 function App() {
 
   const user = useRecoilValue(userAtom);
+  const searchQuery = useRecoilValue(searchQueryAtom);
 
   return (
     <>
       <Navbar ></Navbar>
       <div className="container mx-auto p-4">
+      {searchQuery &&<SearchResults />}
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/signup' element={<SignUp />}/>
           <Route path='/login' element={<Login />}/>
+          <Route path="/contact" element={<Contact />}/>
           <Route path="/profile" element={user ? <UserProfile /> : <Navigate to="/login" />} />
         </Routes>
       </div>
