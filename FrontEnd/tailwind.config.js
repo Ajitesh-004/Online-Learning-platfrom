31e5b0bccdf6d87101ea9b96cd9592c02697b61e
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-12 19:38:54 +0530

    Dependencies

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}", // Include all JS and TS files in the src directory
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        rotate: 'rotate 8s linear infinite', // Define the rotate animation
+      },
+      keyframes: {
+        rotate: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
+      },
+    },
   },
   plugins: [],
 }
