042f4b97f584540ff69d047deb411908af1042a8
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-26 19:22:16 +0530

    Updated courses model with ajitesh model

--- BackEnd/models/courses.js
+++ BackEnd/models/courses.js
@@ -1,19 +1,34 @@
 import mongoose from "mongoose";
 
-const coursesSchema = mongoose.Schema({
+const courseSchema = mongoose.Schema({
   title: { type: String, required: true },
+  category: { type: String, required: true },
   description: { type: String, required: true },
-  price: { type: Number, required: true },
-  thumnbnail: { type: String, required: true },
-  rating: { type: Number, min: 0, max: 5, default: 0 },
-  reviews: [
-    {
-      User: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-      rating: { type: Number, min: 0, max: 5 },
-    },
-  ],
+  duration: {
+    weeks: { type: Number, required: true },
+    hoursOfContent: { type: Number, required: true },
+  },
+  difficultyLevel: { type: String, enum: ["Beginner", "Intermediate", "Advanced", "Beginner to Advanced"], required: true },
+  modules: [{ type: String, required: true }],
+  ratings: {
+    average: { type: Number, min: 0, max: 5, default: 0 },
+    reviews: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        comment: { type: String },
+        rating: { type: Number, min: 0, max: 5 },
+      },
+    ],
+  },
+  enrollments: { type: Number, default: 0 },
+  price: {
+    regular: { type: Number, required: true },
+    discounted: { type: Number },
+    discountValidity: { type: Date },
+  },
+  thumbnail: { type: String },
 });
 
-const Courses = mongoose.model("Courses", coursesSchema);
+const Course = mongoose.model("Course", courseSchema);
 
-export default Courses;
+export default Course;
