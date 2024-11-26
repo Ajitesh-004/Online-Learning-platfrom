adeee3258755260784d7f1b9ad13b28c047790e1
Ashutosh Shukla <ashutoshshukla8970@gmail.com>
2024-11-26 23:33:20 +0530

    Updated courses atom to handle reload as well

--- FrontEnd/src/atoms/coursesAtom.jsx
+++ FrontEnd/src/atoms/coursesAtom.jsx
@@ -1,6 +1,20 @@
 import { atom } from "recoil";
 
+// Helper to load persisted data from localStorage
+const loadCourses = () => {
+  const savedCourses = localStorage.getItem("courses");
+  return savedCourses ? JSON.parse(savedCourses) : [];
+};
+
 export const coursesAtom = atom({
-    key: "coursesAtom", // Unique key for this atom
-    default: [], 
-})
+  key: "coursesAtom", // Unique key for this atom
+  default: loadCourses(), // Load persisted data as the default value
+  effects: [
+    ({ onSet }) => {
+      // Persist data to localStorage whenever the atom is updated
+      onSet((newCourses) => {
+        localStorage.setItem("courses", JSON.stringify(newCourses));
+      });
+    },
+  ],
+});
